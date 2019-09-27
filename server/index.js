9e01f845f6154cb5b4684e21bf01c0eb00722418
Tomás Jiménez <t.jimenez03@ufromail.cl>
2019-09-26 23:56:43 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,15 +5,16 @@
  * 
  * Para inicializar el servidor, utilizar comando "node index.js"
  * 
- * Para comenzar con el streaming, primero, inicializar un listening con ffmpeg y netcat (sudo apt-get install ffmpeg netcat)
+ * Para comenzar con el streaming, primero, inicializar un listening con ffmpeg y netcat, que escuchará el puerto 2222 (sudo apt-get install ffmpeg netcat)
  * : nc -l 2222 | fffmpeg -i - -c copy -f flv rtmp://localhost/live/video <- entrada al servidor rtmp
  * 
- * Para inicializar el streaming, se comienza a grabar y enviar via netcat:
+ * Para inicializar el streaming, se comienza a grabar y enviar via netcat por puerto 2222:
  * : raspivid -t 0 -w 500 -h 500 -fps 20 -o - | nc 0.0.0.0 2222
  */
 const NodeMediaServer = require('node-media-server');
 const express = require('express');
 const cors = require('cors');
+const exec = require('child_process').exec;
 
 
 // Configuración de express
@@ -28,6 +29,19 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send({response:true});
 });
+// Ruta de inicio de streaming de camara de drone.
+app.post("/start_streaming_drone, (req,res) => {
+    // se inicia el Listen de ffmpeg del video de raspivid y se redirecciona a rtmp de node media server
+    exec("nc -l 2222 | ffmpeg -i - -c copy -f flv rtmp://localhost/live/video", () => { //no hacer nada luego de ejecutar el comando });
+    // raspivid -t 0 -w 500 -h 500 -fps 20 -o - | nc 0.0.0.0 2222
+    exec("nc -l 2222 | ffmpeg -i - -c copy -f flv rtmp://localhost/live/video", () => { //no hacer nada luego de ejecutar el comando });
+});
+// Ruta de inicio de streaming de camara de central -> rtmp://localhost/live/videocentral
+app.post("/start_streaming_central, (req,res) => {
+    // se inicia el Listen de ffmpeg del video de camara central (ip servidor de la camara, no de la raspberry de la central) y se redirecciona a rtmp de node media server
+    exec("comm = 'ffmpeg -y -r 1 -i rtmp://192.168.8.160:1935/flash/11:admin:admin -c copy -r 1 -f flv rtmp://localhost/live/videocentral", () => { //no hacer nada luego de ejecutar el comando });
+});    
+    
 
 // Configuracion de node media server para streaming
 /**
@@ -49,3 +63,7 @@ const config = {
 
 var nms = new NodeMediaServer(config)
 nms.run();
+
+app.listen(3000, () => {
+    console.log("Servidor API REST inicializado en puerto :3000");
+});
